deb3886aa6e52bfd5ce4bc0d69d63b50ac4912b1
zbigniewProjects <zbigniew.stiasny@gmail.com>
2024-05-07 05:16:14 +0200

    First commit

---
+++ Pages/Projects/easyuploader.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPortfolio.Pages.Projects
+{
+    public class dnuploaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
